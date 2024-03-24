da3b82d2828fd868836541b4ff027379bdecfd1c
HikaruRyu <152208710+HikaruRyu@users.noreply.github.com>
2024-03-24 21:34:01 +0100

    Add files via upload

---
+++ lsn5unittesting/test/CarTest.java
@@ -0,0 +1,36 @@
+package org.launchcode.java.demos.lsn5unittesting.test;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.java.demos.lsn5unittesting.main.Car;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class CarTest {
+
+    private Car test_car;
+
+    @Before
+    public void createCarObject() {
+        test_car = new Car("Toyota", "Prius", 10, 50);
+    }
+
+    @Test
+    public void testGasTankAfterDriving() {
+        test_car.drive(50);
+        assertEquals(9, test_car.getGasTankLevel(), .001);
+    }
+
+    @Test
+    public void testGasTankAfterExceedingTankRange() {
+        test_car.drive(600); // Attempt to drive more than tank range
+        assertEquals(0, test_car.getGasTankLevel(), .001);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGasOverfillException() {
+        test_car.addGas(5);
+        fail("Shouldn't get here, car cannot have more gas in tank than the size of the tank");
+    }
+}
